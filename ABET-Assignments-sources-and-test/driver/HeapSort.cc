79a377ac48368841aba56ddc06bf2fefdc4687d3
Ashley <ashley.oliveiraa@outlook.com>
2024-07-16 12:44:01 -0400

    Updates HeapSort.cc

--- ABET-Assignments-sources-and-test/driver/HeapSort.cc
+++ ABET-Assignments-sources-and-test/driver/HeapSort.cc
@@ -12,12 +12,124 @@ class Sort::ImplSort
   /* ----- YOUR CODE GOES IN BETWEEN THESE COMMENTS ----- */
   /* vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv */
 
+private:
+  struct Node
+  {
+    Node *left;
+    Node *right;
+    std::string data;
+
+    Node(const std::string Data, Node *left = nullptr, Node *right = nullptr)
+    {
+      data = Data;
+      left = nullptr;
+      right = nullptr;
+    }
+
+    ~Node()
+    {
+      delete left;
+      delete right;
+    }
+
+    void insert(const std::string &value)
+    {
+      if (value < data)
+      {
+        if (left)
+        {
+          left->insert(value);
+          if (left->data > data)
+          {
+            std::swap(left->data, data);
+          }
+        }
+        else
+        {
+          left = new Node(value);
+        }
+      }
+      else
+      {
+        if (right)
+        {
+          right->insert(value);
+          if (right->data > data)
+          {
+            std::swap(right->data, data);
+          }
+        }
+        else
+        {
+          right = new Node(value);
+        }
+      }
+    }
+
+    void heapify()
+    {
+      Node *largest = this;
+      if (left && left->data > largest->data)
+      {
+        largest = left;
+      }
+      if (right && right->data > largest->data)
+      {
+        largest = right;
+      }
+      if (largest != this)
+      {
+        std::swap(data, largest->data);
+        largest->heapify();
+      }
+    }
+
+    void heapSort()
+    {
+      if (!left && !right)
+        return;
+      heapify();
+      if (left)
+        left->heapSort();
+      if (right)
+        right->heapSort();
+    }
+
+    void enumerate(std::ostream &output_file)
+    {
+      if (left)
+        left->enumerate(output_file);
+      output_file << data << "\n";
+      if (right)
+        right->enumerate(output_file);
+    }
+  };
+
+  Node *root = nullptr;
+
+public:
+  void insert(const std::string &key, const std::string &value)
+  {
+    if (root == nullptr)
+    {
+      root = new Node(value, nullptr, nullptr);
+    }
+    else
+      root->insert(value);
+  }
+  void enumerate(std::ostream &output_file)
+  {
+    if (root)
+    {
+      root->heapSort();
+      root->enumerate(output_file);
+    }
+  }
+
   /* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */
   /* ----- YOUR CODE GOES IN BETWEEN THESE COMMENTS ----- */
   /* -----      DO NOT MODIFY BELOW THIS LINE       ----- */
-
 };
-
 
 Sort::Sort() : implSort_(new ImplSort) {}
 
@@ -26,12 +138,12 @@ Sort::~Sort()
   delete implSort_;
 }
 
-void Sort::insert(const std::string & key, const std::string & value)
+void Sort::insert(const std::string &key, const std::string &value)
 {
-  implSort_->insert(key,value);
+  implSort_->insert(key, value);
 }
 
-void Sort::enumerate(std::ostream& output_file)
+void Sort::enumerate(std::ostream &output_file)
 {
   implSort_->enumerate(output_file);
 }
--- ABET-Assignments-sources-and-test/driver/TrieSort.cc
+++ ABET-Assignments-sources-and-test/driver/TrieSort.cc
@@ -12,12 +12,82 @@ class Sort::ImplSort
   /* ----- YOUR CODE GOES IN BETWEEN THESE COMMENTS ----- */
   /* vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv */
 
+private:
+  struct Node
+  {
+    Node *left;
+    Node *right;
+    std::string data;
+
+    Node(std::string Data, Node *left = nullptr, Node *right = nullptr)
+    {
+      data = Data;
+      left = nullptr;
+      right = nullptr;
+    }
+
+    ~Node()
+    {
+      delete left;
+      delete right;
+    }
+
+    void insert(const std::string &key, const std::string &value)
+    {
+      if (value < this->data)
+      {
+        if (left)
+        {
+          left->insert(key, value);
+        }
+        else
+        {
+          left = new Node(std::move(value), nullptr, nullptr);
+        }
+      }
+      else
+      {
+        if (right)
+        {
+          right->insert(key, value);
+        }
+        else
+        {
+          right = new Node(std::move(value), nullptr, nullptr);
+        }
+      }
+    }
+
+    void enumerate(std::ostream &output_file)
+    {
+      if (left)
+        left->enumerate(output_file);
+      output_file << data << "\n";
+      if (right)
+        right->enumerate(output_file);
+    }
+  };
+
+  Node *root = nullptr;
+
+public:
+  void insert(const std::string &key, const std::string &value)
+  {
+    if (root == nullptr)
+    {
+      root = new Node(value, nullptr, nullptr);
+    }
+    else
+      root->insert(key, value);
+  }
+  void enumerate(std::ostream &output_file)
+  {
+    root->enumerate(output_file);
+  }
   /* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */
   /* ----- YOUR CODE GOES IN BETWEEN THESE COMMENTS ----- */
   /* -----      DO NOT MODIFY BELOW THIS LINE       ----- */
-
 };
-
 
 Sort::Sort() : implSort_(new ImplSort) {}
 
@@ -26,15 +96,12 @@ Sort::~Sort()
   delete implSort_;
 }
 
-void Sort::insert(const std::string & key, const std::string & value)
+void Sort::insert(const std::string &key, const std::string &value)
 {
-  implSort_->insert(key,value);
+  implSort_->insert(key, value);
 }
 
-void Sort::enumerate(std::ostream& output_file)
+void Sort::enumerate(std::ostream &output_file)
 {
   implSort_->enumerate(output_file);
 }
-
-
-
